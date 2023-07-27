9006a683241fcbee3354e35fccbe8bb7c10ac70c
asipita <sa.abdulgafar@gmail.com>
2023-07-27 08:49:42 +0100

    briefcase icon set to currentColor

--- components/icons/briefcase.jsx
+++ components/icons/briefcase.jsx
@@ -7,7 +7,7 @@ export const Briefcase = (props) => (
     {...props}
   >
     <path
-      fill={props.color || "#FFF"}
+      fill="currentColor"
       d="M20 6h-3V4c0-1.103-.897-2-2-2H9c-1.103 0-2 .897-2 2v2H4c-1.103 0-2 .897-2 2v3h20V8c0-1.103-.897-2-2-2ZM9 4h6v2H9V4Zm5 10h-4v-2H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-8v2Z"
     />
   </svg>

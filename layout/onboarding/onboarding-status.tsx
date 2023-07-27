5c2bb88493b4efa2c7c753f79224ebd10d93887f
asipita <sa.abdulgafar@gmail.com>
2023-07-27 08:49:20 +0100

    onboarding status ui fix

--- layout/onboarding/onboarding-status.tsx
+++ layout/onboarding/onboarding-status.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import OnboardingPendingStatus from "@/public/onboarding-pending-status.svg";
 import { useGetCurrentUser } from "@/api/hooks/user";
-import { Loader } from "@mantine/core";
+import { Center, Loader, Stack, Text } from "@mantine/core";
+import { Verify } from "iconsax-react";
 
 export function OnboardingStatus() {
   const { data, isLoading } = useGetCurrentUser();
@@ -11,16 +11,21 @@ export function OnboardingStatus() {
     <section className="h-full">
       <div className="flex h-full flex-col items-center justify-center w-[305px] mx-auto">
         {!data?.data.is_approved ? (
-          <>
+          <Stack align="center" spacing="xl">
             <OnboardingPendingStatus />
-            <div className="text-gray-90 text-2xl font-semibold text-center">
+            <div className="text-gray-90 text-2xl font-semibold text-center font-secondary">
               Please wait while we Approve your Account
             </div>
-          </>
+          </Stack>
         ) : (
-          <div className="text-gray-90 text-2xl font-semibold text-center">
-            Your account has been approved
-          </div>
+          <Center className="h-full">
+            <Stack align="center" spacing="xl">
+              <Verify size="120" variant="Bold" color="green" />
+              <Text className="font-secondary" size="xl" align="center">
+                Your account has been successfully verified
+              </Text>
+            </Stack>
+          </Center>
         )}
       </div>
     </section>

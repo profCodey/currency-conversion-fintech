3124d4f35a8b2c1510417af9770c6cff549ce737
profCodey <otunaiyademilade@gmail.com>
2023-10-10 10:38:31 +0100

    redirect homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,79 @@
-import Banner from "@/layout/home/banner";
-import { Benefits } from "@/layout/home/benefits";
-import { Advantages } from "@/layout/home/advantages";
-import { Navbar } from "@/layout/common/navbar";
-import { Uses } from "@/layout/home/uses";
-import { HowWeProtect } from "@/layout/home/how-we-protect";
-import { Faq } from "@/layout/home/faq";
-import { Footer } from "@/layout/common/footer";
+import { useLogin } from "@/api/hooks/auth";
+import AuthLayout from "@/layout/auth/auth-layout";
+import { loginFormValidator } from "@/utils/validators";
+import { TextInput, PasswordInput, Stack, Button } from "@mantine/core";
+import { useForm, zodResolver } from "@mantine/form";
+import Link from "next/link";
+import { ReactElement } from "react";
+import { z } from "zod";
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
 
-export default function Home() {
+export default function Login() {
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   // Redirect to the login page when the component mounts
+  //   router.push('/login');
+  // }, []);
+
+  const { mutate: login, isLoading } = useLogin();
+  const loginForm = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: zodResolver(loginFormValidator),
+  });
+
+  function handleSubmit(values: z.infer<typeof loginFormValidator>) {
+    login(values);
+  }
+
   return (
-    <>
-      <Navbar />
-      <main className="">
-        <Banner />
-        <Benefits />
-        <Advantages />
-        <Uses />
-        <HowWeProtect />
-        <Faq />
-      </main>
-      <Footer />
-    </>
+    <form className="w-full" onSubmit={loginForm.onSubmit(handleSubmit)}>
+      <Stack spacing="lg">
+        <TextInput
+          placeholder="Enter email address"
+          size="lg"
+          {...loginForm.getInputProps("email")}
+        />
+        <PasswordInput
+          placeholder="Password"
+          size="lg"
+          {...loginForm.getInputProps("password")}
+        />
+
+        <Link href="/forgot-password">
+          <small className="text-red-700 text-sm">Forgot Password?</small>
+        </Link>
+        <Button
+          type="submit"
+          size="lg"
+          className="mt-1 bg-[#132144] hover:bg-[#132144] font-secondary"
+          loading={isLoading}
+        >
+          Login
+        </Button>
+
+        <div>
+          Not registered yet?{" "}
+          <Link className="text-blue-700" href="/sign-up">
+            Sign up
+          </Link>
+        </div>
+      </Stack>
+    </form>
   );
 }
+
+Login.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <AuthLayout
+      title="Hi, Welcome Back!"
+      subtitle="Please enter your details to login."
+    >
+      {page}
+    </AuthLayout>
+  );
+};
---
+++ pages/index2.tsx
@@ -0,0 +1,25 @@
+import Banner from "@/layout/home/banner";
+import { Benefits } from "@/layout/home/benefits";
+import { Advantages } from "@/layout/home/advantages";
+import { Navbar } from "@/layout/common/navbar";
+import { Uses } from "@/layout/home/uses";
+import { HowWeProtect } from "@/layout/home/how-we-protect";
+import { Faq } from "@/layout/home/faq";
+import { Footer } from "@/layout/common/footer";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="">
+        <Banner />
+        <Benefits />
+        <Advantages />
+        <Uses />
+        <HowWeProtect />
+        <Faq />
+      </main>
+      <Footer />
+    </>
+  );
+}

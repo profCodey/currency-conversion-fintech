7b6c409ee1ef29f4b940b334146d0fef0a290cb9
profCodey <otunaiyademilade@gmail.com>
2023-10-12 08:29:43 +0100

    redirected homepage to login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,34 @@
-import { useLogin } from "@/api/hooks/auth";
-import AuthLayout from "@/layout/auth/auth-layout";
-import { loginFormValidator } from "@/utils/validators";
-import { TextInput, PasswordInput, Stack, Button } from "@mantine/core";
-import { useForm, zodResolver } from "@mantine/form";
-import Link from "next/link";
-import { ReactElement } from "react";
-import { z } from "zod";
+// import Banner from "@/layout/home/banner";
+// import { Benefits } from "@/layout/home/benefits";
+// import { Advantages } from "@/layout/home/advantages";
+// import { Navbar } from "@/layout/common/navbar";
+// import { Uses } from "@/layout/home/uses";
+// import { HowWeProtect } from "@/layout/home/how-we-protect";
+// import { Faq } from "@/layout/home/faq";
+// import { Footer } from "@/layout/common/footer";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-export default function Login() {
+export default function Home() {
+  const router = useRouter();
 
-  const { mutate: login, isLoading } = useLogin();
-  const loginForm = useForm({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    validate: zodResolver(loginFormValidator),
-  });
+  useEffect(() => {
+    router.push('/login');
+  }, [router]); 
 
-  function handleSubmit(values: z.infer<typeof loginFormValidator>) {
-    login(values);
-  }
-
-  return (
-    <form className="w-full" onSubmit={loginForm.onSubmit(handleSubmit)}>
-      <Stack spacing="lg">
-        <TextInput
-          placeholder="Enter email address"
-          size="lg"
-          {...loginForm.getInputProps("email")}
-        />
-        <PasswordInput
-          placeholder="Password"
-          size="lg"
-          {...loginForm.getInputProps("password")}
-        />
-
-        <Link href="/forgot-password">
-          <small className="text-red-700 text-sm">Forgot Password?</small>
-        </Link>
-        <Button
-          type="submit"
-          size="lg"
-          className="mt-1 bg-[#132144] hover:bg-[#132144] font-secondary"
-          loading={isLoading}
-        >
-          Login
-        </Button>
-
-        <div>
-          Not registered yet?{" "}
-          <Link className="text-blue-700" href="/sign-up">
-            Sign up
-          </Link>
-        </div>
-      </Stack>
-    </form>
-  );
+  return null;
+  // return (
+  //   <>
+  //     <Navbar />
+  //     <main className="">
+  //       <Banner />
+  //       <Benefits />
+  //       <Advantages />
+  //       <Uses />
+  //       <HowWeProtect />
+  //       <Faq />
+  //     </main>
+  //     <Footer />
+  //   </>
+  // );
 }
-
-Login.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <AuthLayout
-      title="Hi, Welcome Back!"
-      subtitle="Please enter your details to login."
-    >
-      {page}
-    </AuthLayout>
-  );
-};

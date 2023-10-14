d4726ea27fb9dfd6d56caa74f6ce5911b78e0e2b
profCodey <otunaiyademilade@gmail.com>
2023-10-14 16:40:44 +0100

    tawkto chat integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,7 @@
+//@ts-nocheck
+//types has been ignored on page to accomodate tawk.to implementation. Remove ts-nocheck when tawk.to supports typescript
+//Refer to page below
+//https://github.com/tawk/tawk-messenger-react/issues/13
 import "@/styles/globals.css";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -11,6 +15,8 @@ import { ReactElement, ReactNode } from "react";
 import { montserrat, sora, openSans } from "@/utils/fonts";
 import { ModalsProvider } from "@mantine/modals";
 import { RouterTransition } from "@/components/router-transitions";
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react'; 
+import { TAWKTO_KEYS } from "@/utils/constants";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -58,6 +64,10 @@ export default function App(props: AppPropsWithLayout) {
               className={`${montserrat.variable} ${sora.variable} ${openSans.variable} font-primary`}
             >
               {getLayout(<Component {...pageProps} />)}
+              <TawkMessengerReact
+                propertyId= {TAWKTO_KEYS.PROPERTY_ID}
+                widgetId= {TAWKTO_KEYS.WIDGET_ID}
+                />
             </section>
           </ModalsProvider>
           <ReactQueryDevtools initialIsOpen={false} />
--- utils/constants.ts
+++ utils/constants.ts
@@ -22,3 +22,8 @@ export const APICLIENT_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://mabillagroupappslb-1683373620.eu-west-1.elb.amazonaws.com:8080/"
     : "http://mabillagroupappslb-1683373620.eu-west-1.elb.amazonaws.com:8080/";
+
+    export const TAWKTO_KEYS ={
+      PROPERTY_ID : "63c61ee847425128790e04b6",
+      WIDGET_ID : "1hcnbd2e3"
+    }

9a85e13379d4a2acbb54dc3327e524291c0de710
Sherif Adigun <sherif.adigun@payceler.com>
2023-10-14 15:50:28 +0000

    constants.ts edited online with Bitbucket

--- utils/constants.ts
+++ utils/constants.ts
@@ -23,7 +23,7 @@ export const APICLIENT_BASE_URL =
     ? "http://mabillagroupappslb-1683373620.eu-west-1.elb.amazonaws.com:8080/"
     : "http://mabillagroupappslb-1683373620.eu-west-1.elb.amazonaws.com:8080/";
 
-    export const TAWKTO_KEYS ={
-      PROPERTY_ID : "63c61ee847425128790e04b6",
-      WIDGET_ID : "1hcnbd2e3"
-    }
+export const TAWKTO_KEYS = {
+  PROPERTY_ID : "63c61ee847425128790e04b6",
+  WIDGET_ID : "1hcnbd2e3"
+}

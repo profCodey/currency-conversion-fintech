4b9a1379c7065b8081e9f716bae741e8c672dca4
profCodey <otunaiyademilade@gmail.com>
2023-11-24 12:55:59 +0100

    fixed destination dropdown to list all currencies

--- pages/admin/transactions.tsx
+++ pages/admin/transactions.tsx
@@ -9,8 +9,15 @@ import { FxPayoutHistory } from "@/layout/transactions/fx-payout-history";
 import { Tabs } from "@mantine/core";
 import { ReactElement } from "react";
 import Cookies from "js-cookie";
+import { useGetSiteSettings } from "@/api/hooks/admin/sitesettings";
+import { ISiteSettings } from "@/utils/validators/interfaces";
 
 export default function Transactions() {
+  const { data: siteSettings, isLoading: siteSettingsLoading } =
+  useGetSiteSettings();
+
+const settings: ISiteSettings | undefined = siteSettings?.data;
+console.log("settings", settings);
 
   return (
     <section className="flex flex-col gap-6 h-full">
@@ -34,8 +41,8 @@ export default function Transactions() {
             <Tabs.Tab value="payouts">Local Payouts</Tabs.Tab>
             <Tabs.Tab value="fx-payouts">Foreign Payouts</Tabs.Tab>
             <Tabs.Tab value="fundings">Fundings</Tabs.Tab>
-            <Tabs.Tab value="exchange">Exchange</Tabs.Tab>
-            {/* <Tabs.Tab value="fx">Foreign Transactions</Tabs.Tab> */}
+            {!settings?.use_fx_wallet && <Tabs.Tab value="exchange">Exchange</Tabs.Tab>}
+            <Tabs.Tab value="fx">Foreign Transactions</Tabs.Tab>
           </Tabs.List>
           <Tabs.Panel value="fx-payouts" className="pt-5">
             <FxPayoutHistory />

104d1bcb24458e5f2933d64444c3f0223d082d35
profCodey <otunaiyademilade@gmail.com>
2024-02-21 12:07:25 +0100

    remove console.log

--- api/hooks/banks.tsx
+++ api/hooks/banks.tsx
@@ -326,15 +326,12 @@ export function useDeleteAccount(cb?: () => void) {
 }
 
 export function useGetManualFundings(category: string) {
-    const res = useQuery({
+    return useQuery({
         queryKey: ["manual-fundings"],
         queryFn: function (): Promise<AxiosResponse<IManualPayment[]>> {
             return axiosInstance.get(`/manual-funding?category=${category}`);
         },
     });
-
-    console.log('res', res)
-    return res
 }
 
 export function usePostManualFunding(cb?: () => void) {
